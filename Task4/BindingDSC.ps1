487f991d606e07e9576f0ff99bded09e7f1b44a3
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-09 18:18:28 +0300

    4

---
+++ Task4/BindingDSC.ps1
@@ -0,0 +1,20 @@
+﻿Configuration Binding
+{
+$sitename = "Default Web Site" 
+xWebsite MainHTTPWebsite  
+{  
+    Ensure          = "Present"  
+    Name            = $sitename
+    ApplicationPool = "DefaultAppPool" 
+    State           = "Started"  
+    PhysicalPath    = "%SystemDrive%\inetpub\wwwroot\iisstart.htm"  
+    BindingInfo     =
+                        @(MSFT_xWebBindingInformation
+                            {
+                                Protocol              = "HTTP"
+                                Port                  = 8080
+                                HostName              = "http://localhost/"
+                            }
+                        )
+}
+}
