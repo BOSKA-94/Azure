8493669e54fb481c73d8244ce1347059ad519579
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-10 17:57:55 +0300

    Task5

--- Task4/DSC.ps1
+++ Task4/DSC.ps1
@@ -1,14 +1,23 @@
 Configuration Task 
 {
     param ($MachineName)
-    $sitename = "Default Web Site" 
 
-    Import-DscResource -ModuleName PsDesiredStateConfiguration 
-    Import-DscResource -ModuleName xWebadministration
+    Import-DscResource -ModuleName xNetworking, xWebAdministration, PSDesiredStateConfiguration
 
     Node $MachineName {
 
-        WindowsFeature WebServer {
+        xFirewall IIS_8080 {
+            Name        = 'IIS'
+            DisplayName = 'IIS'
+            Action      = 'Allow'
+            Direction   = 'Inbound'
+            LocalPort   = '8080'
+            Protocol    = 'TCP'
+            Profile     = 'Any'
+            Enabled     = 'True'
+        }
+
+        WindowsFeature IIS {
             Ensure = "Present"
             Name   = "Web-Server"
         }
@@ -16,23 +25,27 @@ Configuration Task
         WindowsFeature ASP {
             Ensure    = "Present"
             Name      = "Web-Asp-Net45"
-            DependsOn = "[WindowsFeature ]WebServer"
+            DependsOn = '[WindowsFeature]IIS'
         }
+
+        WindowsFeature WebServerManagementConsole {
+            Name   = "Web-Mgmt-Console"
+            Ensure = "Present"
+        }
+        
         xWebsite MainHTTPWebsite {  
             Ensure          = "Present"  
-            Name            = $sitename
+            Name            = 'Default Web Site'
             ApplicationPool = "DefaultAppPool" 
             State           = "Started"  
-            PhysicalPath    = "%SystemDrive%\inetpub\wwwroot\iisstart.htm"  
+            PhysicalPath    = 'C:\inetpub\wwwroot'  
             BindingInfo     = @(MSFT_xWebBindingInformation {
-                    Protocol  = 'HTTP'
+                    Protocol  = 'HTTP' 
                     Port      = '8080'
                     IPAddress = '*'
-                    HostName  = 'http://localhost/'
                 }
             )
-            DependsOn       = "[WindowsFeature ]WebServer"
+            DependsOn       = "[WindowsFeature]ASP"    
         }
     }
 }
-
