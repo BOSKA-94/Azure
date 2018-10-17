8e8b850b8530b9e1c89150c5837408d55ed38f19
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-17 19:30:49 +0300

    Tasks

--- Task8/Main.ps1
+++ Task8/Main.ps1
@@ -3,27 +3,26 @@ param(
     $subscriptionId = "4712ca89-5e08-4d78-9f70-35f10bec642b",
 
     [string]
-    $resourceGroupName = "MC_Minsk_Task8_northeurope",
+    $resourceGroupName = "Minsk",
 
     [string]
     $resourceGroupLocation,
 
     [string]
-    $KeyVaultPath = "C:\Azure\Task8\KeyVault.json",
+    $KeyVaultPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task8/KeyVault.json",
 
     [string]
-    $KeyVaultParPath = "C:\Azure\Task8\KeyVaultPar.json",
+    $KeyVaultParPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task8/KeyVaultPar.json",
 
     [string]
-    $mainPath = "C:\Azure\Task8\main.json",
+    $mainPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task8/main.json",
 
     [Parameter(Mandatory = $True)]
     [SecureString]
     $secretvalue
 )
-#Connect-AzureRmAccount
-#Select-AzureRmSubscription -SubscriptionName $subscriptionId
+Connect-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName $subscriptionId
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $KeyVaultPath -TemplateParameterFile $KeyVaultParPath;
 Set-AzureKeyVaultSecret -VaultName 'Task8' -Name 'DevOps' -SecretValue $secretvalue;
-New-AzureRmResourceGroup -Name MC_Minsk_Task8_northeurope -Location "North Europe"
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile C:\Azure\Task8\main.json -Verbose;
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile C:\Azure\Task8\main.json;
