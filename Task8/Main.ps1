7be5aec7b0b58f29c3258682d548a51459d596d0
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-17 17:51:47 +0300

    Tasks

--- Task8/Main.ps1
+++ Task8/Main.ps1
@@ -3,7 +3,7 @@ param(
     $subscriptionId = "4712ca89-5e08-4d78-9f70-35f10bec642b",
 
     [string]
-    $resourceGroupName = "Minsk",
+    $resourceGroupName = "MC_Minsk_Task8_northeurope",
 
     [string]
     $resourceGroupLocation,
@@ -25,4 +25,5 @@ param(
 #Select-AzureRmSubscription -SubscriptionName $subscriptionId
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $KeyVaultPath -TemplateParameterFile $KeyVaultParPath;
 Set-AzureKeyVaultSecret -VaultName 'Task8' -Name 'DevOps' -SecretValue $secretvalue;
-New-AzureRmResourceGroupDeployment -ResourceGroupName MC_Minsk_Task8_northeurope -TemplateFile C:\Azure\Task8\main.json -Verbose;
+New-AzureRmResourceGroup -Name MC_Minsk_Task8_northeurope -Location "North Europe"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile C:\Azure\Task8\main.json -Verbose;
