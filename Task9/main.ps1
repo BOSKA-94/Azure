b33d21b24a082e567bf343795be2ff6e577bdb3e
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-20 12:52:12 +0300

    Tasks

--- Task9/main.ps1
+++ Task9/main.ps1
@@ -12,19 +12,19 @@ param(
     $deploymentName = "Task9",
 
     [string]
-    $KeyVaultPath = "C:\Azure\Task9\KeyVault.json",
+    $KeyVaultPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/KeyVault.json",
 
     [string]
-    $KeyVaultParPath = "C:\Azure\Task9\KeyVaultPar.json",
+    $KeyVaultParPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/KeyVaultPar.json",
 
     [string]
-    $VM_VNetPath = "C:\Azure\Task9\Vm_VNet.json",
+    $VM_VNetPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/Vm_VNet.json",
 
     [string]
-    $AutomationPath = "C:\Azure\Task9\Automation.json",
+    $AutomationPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/Automation.json",
 
     [string]
-    $AutomationParPath = "C:\Azure\Task9\AutomationPar.json",
+    $AutomationParPath = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/AutomationPar.json",
 
     [Parameter(Mandatory = $True)]
     [SecureString]
@@ -76,13 +76,13 @@ else {
 
 # Start the deployment Key Vault
 Write-Host "Starting deployment Key Vault...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $KeyVaultPath -TemplateParameterFile $KeyVaultParPath;
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $KeyVaultPath -TemplateParameterUri $KeyVaultParPath;
 Set-AzureKeyVaultSecret -VaultName 'Task9' -Name 'epam' -SecretValue $secretvalue
 
 
 # Start the deployment Vm and VNet
 Write-Host "Starting deployment VM and Vnet...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $Vm_VNetPath;
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $Vm_VNetPath;
 
 
 # Start the deployment Automation Account
@@ -94,4 +94,4 @@ $ObjectId = $AppRegestration.ObjectId
 $AppliccationId = $AppRegestration.ApplicationId
 New-AzureRmADAppCredential -ObjectId $ObjectId -Password $password
 Write-Host "Starting deployment Automation Account...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $AutomationPath -TemplateParameterFile $AutomationParPath -password $password -userName $AppliccationId -JobId $JobGUID 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $AutomationPath -TemplateParameterUri $AutomationParPath -password $password -userName $AppliccationId -JobId $JobGUID 
