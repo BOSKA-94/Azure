bcd15b950c90d4360994bff0229b2be9febdb278
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-10-22 18:46:45 +0300

    Tasks

--- Task9/123.ps1
+++ Task9/123.ps1
@@ -14,20 +14,6 @@ param(
     [string]
     $KeyVault_VNet = "https://raw.githubusercontent.com/BOSKA-94/Azure/master/Task9/KeyVault_VNet.json",
 
-    [string]
-    $AutomationPath = "C:\Azure\Task9\Automation.json",
-
-    [string]
-    $VM1 = "C:\Azure\Task9\VM.json",
-
-    [string]
-    $VMParPath1 = "C:\Azure\Task9\VMpar.json",
-
-    [string]
-    $VM2 = "C:\Azure\Task9\VM2.json",
-
-    [string]
-    $VMParPath2 = "C:\Azure\Task9\VMpar2.json",
 
     [Parameter(Mandatory = $True)]
     [SecureString]
@@ -46,11 +32,11 @@ $ErrorActionPreference = "Stop"
 
 # sign in
 Write-Host "Logging in...";
-Login-AzureRmAccount;
+#Login-AzureRmAccount;
 
 # select subscription
 Write-Host "Selecting subscription '$subscriptionId'";
-Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+#Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
 # Register RPs
 $resourceProviders = @("microsoft.keyvault");
@@ -91,21 +77,3 @@ $AppliccationId = $AppRegestration.ApplicationId
 New-AzureRmADAppCredential -ObjectId $ObjectId -Password $secretvalue
 Write-Host "Starting deployment Automation Account...";
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $AutomationPath -password $secretvalue -userName $AppliccationId -JobId $JobGUID 
-$automationAccountKey = ConvertTo-SecureString -AsPlainText ((Get-AzureRmAutomationAccount -ResourceGroupName $resourceGroupName | Get-AzureRmAutomationRegistrationInfo).PrimaryKey) -Force
-$automationAccountUrl = ConvertTo-SecureString -AsPlainText ((Get-AzureRmAutomationAccount -ResourceGroupName $resourceGroupName | Get-AzureRmAutomationRegistrationInfo).Endpoint) -Force
-
-
-#DSC
-$AutomationAccountName = (Get-AzureRmAutomationAccount).AutomationAccountName
-Import-AzureRmAutomationDscConfiguration -SourcePath 'C:\Azure\Task9\TestConfig.ps1' -ResourceGroupName $resourceGroupName -AutomationAccountName $AutomationAccountName -Published;
-Start-AzureRmAutomationDscCompilationJob -ConfigurationName 'TestConfig' -ResourceGroupName $resourceGroupName -AutomationAccountName $AutomationAccountName;
-
-
-# Start the deployment Vm with ISS
-Write-Host "Starting deployment Vm with ISS...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM1 -TemplateParameterFile $VMParPath1 -automationAccountKey $automationAccountKey -automationAccountUrl $automationAccountUrl;
-
-# Start the deployment Vm without ISS
-Write-Host "Starting deployment Vm without ISS...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM2 -TemplateParameterFile $VMParPath2 -automationAccountKey $automationAccountKey -automationAccountUrl $automationAccountUrl;
-
