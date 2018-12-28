e88753d7362e1709dbfad771313503f68169a626
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-12-28 15:17:17 +0300

    DSC

--- DSC/DSC.ps1
+++ DSC/DSC.ps1
@@ -2,7 +2,7 @@ Configuration BaseInstall
 {
     param ($MachineName)
     import-DscResource -ModuleName 'PSDesiredStateConfiguration'
-    Node "localhost"
+    Node localhost
     {
         $zipUri = 'https://www.7-zip.org/a/7z1805-x64.msi'
         $JreUri = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=235727_2787e4a523244c269598db4e85c51e0c'
@@ -10,25 +10,12 @@ Configuration BaseInstall
         $JreOut = 'jre-8u191-windows-x64.exe'
         $InstallDir = 'C:\Install'
 
-        #EnableTLS 1.2 
-        Script EnableTLS12 {
-            SetScript  = {
-                [ Net.ServicePointManager ]::SecurityProtocol = [ Net.ServicePointManager ]::SecurityProtocol.toString() + ' , ' + [ Net.SecurityProtocolType ]::Tls12
-            }
-            TestScript = {
-                return ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
-            }
-            GetScript  = {
-                return @{
-                    Result = ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
-                }
-            }
-        }
         Script downloadSoft {
             SetScript  = {
                 If (!(Test-Path -Path "C:\Install" -PathType Container)) { 
                     New-Item -ItemType Directory -Path "C:\" -Name "Install"
                 }
+                [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
                 Invoke-WebRequest -Uri $zipUri -OutFile "C:\Install\7z1805-x64.msi" -UseBasicParsing -Verbose 
             }
             TestScript = {
@@ -40,4 +27,4 @@ Configuration BaseInstall
     }
 }
 BaseInstall -OutputPath C:\mof
-Start-DscConfiguration -Path C:\mof -ComputerName "localhost" -Force -Verbose -wait
+Start-DscConfiguration -Path C:\mof -ComputerName localhost -Force -Verbose -wait
