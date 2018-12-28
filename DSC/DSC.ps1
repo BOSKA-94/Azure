d2b8d875394b683c90fe8d6e6114cc274eb5bbaa
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2018-12-28 14:30:19 +0300

    DSC

--- DSC/DSC.ps1
+++ DSC/DSC.ps1
@@ -1,33 +1,40 @@
 Configuration BaseInstall
 {
     param ($MachineName)
-    import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'xPSDesiredStateConfiguration'
+    import-DscResource -ModuleName 'PSDesiredStateConfiguration'
     Node $MachineName
     {
-            $zipUri = 'https://www.7-zip.org/a/7z1805-x64.msi'
-            $JreUri = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=235727_2787e4a523244c269598db4e85c51e0c'
-            $zipOut = '7z1805-x64.msi'
-            $JreOut = 'jre-8u191-windows-x64.exe'
-            $InstallDir = 'C:\Install'
-#EnableTLS 1.2 
-Script EnableTLS12 {
-    SetScript  = {
-        [ Net.ServicePointManager ]::SecurityProtocol = [ Net.ServicePointManager ]::SecurityProtocol.toString() + ' , ' + [ Net.SecurityProtocolType ]::Tls12
-    }
-    TestScript = {
-        return ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
-    }
-    GetScript  = {
-        return @{
-            Result = ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
+        $zipUri = 'https://www.7-zip.org/a/7z1805-x64.msi'
+        $JreUri = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=235727_2787e4a523244c269598db4e85c51e0c'
+        $zipOut = '7z1805-x64.msi'
+        $JreOut = 'jre-8u191-windows-x64.exe'
+        $InstallDir = 'C:\Install'
+
+        #EnableTLS 1.2 
+        Script EnableTLS12 {
+            SetScript  = {
+                [ Net.ServicePointManager ]::SecurityProtocol = [ Net.ServicePointManager ]::SecurityProtocol.toString() + ' , ' + [ Net.SecurityProtocolType ]::Tls12
+            }
+            TestScript = {
+                return ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
+            }
+            GetScript  = {
+                return @{
+                    Result = ([ Net.ServicePointManager ]::SecurityProtocol -match ' Tls12 ' )
+                }
+            }
+        }
+        Script downloadSoft {
+            SetScript  = {
+                Invoke-WebRequest -Uri $zipUri -OutFile "C:\Install\7z1805-x64.msi" -UseBasicParsing -Verbose 
+            }
+            TestScript = {
+                Test-Path -Path "C:\Install\7z1805-x64.msi"
+            }
+            GetScript  = {
+            }
         }
     }
 }
-#7zip
-xRemoteFile Zip {
-    DestinationPath = "$InstallDir"
-    Uri             = $zipUri
-    MatchSource = $false
-}
-    }
-}
+BaseInstall -OutputPath C:\soft\mof
+Start-DscConfiguration -Path C:\soft\mof -ComputerName $MachineName -Force -Verbose -wait
