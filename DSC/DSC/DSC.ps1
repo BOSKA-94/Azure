cfc42d181648ce9a26926baf9f77c5f478526d7f
Kiryl Baskaulau <Kiryl_Baskaulau@epam.com>
2019-01-09 12:34:48 +0300

    DSC

---
+++ DSC/DSC/DSC.ps1
@@ -0,0 +1,27 @@
+Configuration BaseInstall
+{
+    param ($MachineName)
+    import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'xPSDesiredStateConfiguration'
+    Node $MachineName
+    {
+     #Added Notepad ++ 
+     xRemoteFile Notepad
+     {
+         DestinationPath = "D:\npp.7.5.6.Installer.x64.exe"
+         Uri = "https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.Installer.x64.exe"
+         MatchSource = $false
+     }
+     Package Notepad
+     {
+         Ensure = "Present"
+         Name = "Notepad++ (64-bit x64)"
+         Path = "D:\npp.7.5.6.Installer.x64.exe"
+         ProductId = ''
+         Arguments = "/S"
+         DependsOn = "[xRemoteFile]Notepad"
+         
+     }   
+    }
+}
+BaseInstall
+Start-DscConfiguration .\BaseInstall -ComputerName 'localhost' -Force -Verbose -wait
--- DSC/Script.ps1
+++ DSC/Script.ps1
@@ -5,30 +5,45 @@ param(
     [string]
     $resourceGroupName = "LC-CLAB-RG-DT",
 
+    ################Storage Account################################################
     [string]
-    $resourceGroupLocation,
-################DSC1################################################
-    [string]
-    $VM_DSC_Path = "C:\Users\Kiryl_Baskaulau\Desktop\Azure\DSC\Azure\DSC\VM_DSC.json",
+    $StoragePath = "C:\Users\Kiryl_Baskaulau\Desktop\Azure-1\DSC\VM_DSC.json",
 
     [string]
-    $VM_DSCPar_Path = "C:\Users\Kiryl_Baskaulau\Desktop\Azure\DSC\Azure\DSC\VM_DSCPar.json"
+    $StorageParPath = "C:\Users\Kiryl_Baskaulau\Desktop\Azure-1\DSC\VM_DSCPar.json",
+
+################DSC1################################################
+    [string]
+    $VM_DSC_Path = "C:\Users\Kiryl_Baskaulau\Desktop\Azure-1\DSC\VM_DSC.json",
+
+    [string]
+    $VM_DSCPar_Path = "C:\Users\Kiryl_Baskaulau\Desktop\Azure-1\DSC\VM_DSCPar.json"
 )
+
 $ErrorActionPreference = "Stop"
 
 # sign in
 Write-Host "Logging in...";
-Login-AzureRmAccount;
+Login-AzAccount;
 
 # select subscription
 Write-Host "Selecting subscription '$subscriptionId'";
-Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+Select-AzSubscription -SubscriptionID $subscriptionId;
+
+# Start the deployment Storage account
+Write-Host "Starting deployment Storage account...";
+if (Test-Path $StorageParPath) {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $StoragePath -TemplateParameterFile $StorageParPath;
+}
+else {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $StoragePath;
+}
 
 # Start the deployment DSC
 Write-Host "Starting deployment DSC...";
 if (Test-Path $VM_DSCPar_Path) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM_DSC_Path -TemplateParameterFile $VM_DSCPar_Path;
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM_DSC_Path -TemplateParameterFile $VM_DSCPar_Path;
 }
 else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM_DSC_Path;
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $VM_DSC_Path;
 }
